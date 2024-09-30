a9a8d7ba26c62d95d8d97145ced2b56704387bcf
bluemario8 <bluemario812@gmail.com>
2024-09-30 09:46:30 -0400

    Added Rock button functionality

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,10 +1,55 @@
 let userScore = 0;
-let computerScore = 0;
+let cpuScore = 0;
 let tieScore = 0;
+
+let userScoreElem = document.getElementById("userscore");
+let cpuScoreElem = document.getElementById("cpuscore");
+let tieScoreElem = document.getElementById("tiescore");
+let yourChoiceElem = document.getElementById("yourchoice");
+let cpuChoiceElem = document.getElementById("cpuchoice");
+let resultElem = document.getElementById("result");
+
+
+function getCpuChoice()
+{
+    let rng = Math.floor(Math.random() * 3);
+
+    switch (rng)
+    {
+    case 0:
+        return "rock";
+    case 1:
+        return "paper";
+    case 2:
+        return "scissors";
+    }
+}
 
 function rock()
 {
-    console.log("rock");
+    let cpuChoice = getCpuChoice();
+
+    yourChoiceElem.innerText = "Rock";
+    cpuChoiceElem.innerText = cpuChoice;
+
+    if (cpuChoice === "rock")
+    {
+        resultElem.innerText = "Tie";
+        tieScore++;
+        tieScoreElem.innerText = tieScore;
+    }
+    else if (cpuChoice === "paper")
+    {
+        resultElem.innerText = "Computer Won";
+        cpuScore++;
+        cpuScoreElem.innerText = cpuScore;
+    }
+    else
+    {
+        resultElem.innerText = "You won";
+        userScore++;
+        userScoreElem.innerText = userScore;
+    }
 }
 function paper()
 {
